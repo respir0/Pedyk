17e869f84f9072d081af1233bfc8c3ad56135279
respir0 <kucherev.shura@mail.ru>
2026-05-25 06:56:04 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { getCookie } from '../utils/cookie';
+import { getCookie, deleteCookie } from '../utils/cookie';
 
 const api = axios.create({
   baseURL: '/api/v1',
@@ -31,6 +31,9 @@ api.interceptors.response.use(
         localStorage.removeItem('token');
         localStorage.removeItem('user_login');
         localStorage.removeItem('nickname');
+        localStorage.removeItem('user_id');
+        localStorage.removeItem('about');
+        deleteCookie('access_token');
         window.location.href = '/login';
       }
     }
